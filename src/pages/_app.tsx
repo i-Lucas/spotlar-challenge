fcfc3fd67232b348916fdbcaeb5262680f1151b3
i-Lucas <lucas.oliveira@voltbras.com.br>
2023-05-04 11:55:23 -0300

    feat head component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/reset.css";
 import appSteps from "@/utils/appSteps";
 import type { AppProps } from "next/app";
 import currencyContext from "@/context/currencyContext";
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -21,6 +22,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <currencyContext.Provider value={{ appGlobalContext, setAppGlobalContext }} >
+      <Head>
+        <title>Currency Converter</title>
+        <meta name="description" content="A very efficient currency converter" />
+        <link rel="shortcut icon" href="favicon.png" />
+      </Head>
       <Component {...pageProps} />
     </currencyContext.Provider>
   )
